ea7a96e6cc04b52d0edfa5a738840869666ea47c
sungbin <dusvlf5950@naver.com>
2025-06-23 05:58:10 +0900

    기본 App 컴포넌트 생성 및 환경변수 검증 로직 추가

---
+++ .history/src/App_20250623055801.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import { ChatProvider } from './contexts/ChatContext';
+import { ThemeProvider } from './contexts/ThemeContext';
+import AppRouter from './router/AppRouter';
+import { validateConfig } from './config/app';
+import './styles/globals.scss';
+
+function App() {
+  // 환경변수 검증
+  React.useEffect(() => {
+    const errors = validateConfig();
+    if (errors.length > 0) {
+      console.error('Configuration errors:', errors);
+    }
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <ThemeProvider>
+        <AuthProvider>
+          <ChatProvider>
+            <div className="app">
+              <AppRouter />
+            </div>
+          </ChatProvider>
+        </AuthProvider>
+      </ThemeProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
